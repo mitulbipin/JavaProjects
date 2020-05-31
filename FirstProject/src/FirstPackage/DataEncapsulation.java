7a32a180f249ab9cfc8dda266c19281e59d94b03
Mitul Bipin <mitulbipin24@gmail.com>
2020-05-31 15:43:35 +0530

    Added data encapsulation

---
+++ FirstProject/src/FirstPackage/DataEncapsulation.java
@@ -0,0 +1,33 @@
+package FirstPackage;
+
+class Encapsulate {
+	private String mobileName; 
+	private double price; 
+	
+	protected void setPrice(double price) {
+		this.price = price; 
+	}
+	
+	protected void setName(String mobileName) {
+		this.mobileName = mobileName;
+	}
+	
+	public double getPrice() {
+		return price; 
+	}
+	
+	public String getName() {
+		return mobileName;
+	}
+	
+}
+
+public class DataEncapsulation{
+
+	public static void main(String args[]) {
+		Encapsulate e = new Encapsulate();		
+		e.setPrice(32999.77);e.setName("OnePlus");
+		
+		System.out.println(e.getPrice() +" : "+ e.getName());
+	}
+}
