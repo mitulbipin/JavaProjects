e04b8d162e7efd9af3881d4f72dce40cd6142f66
Mitul Bipin <mitulbipin24@gmail.com>
2020-06-07 01:27:46 +0530

    Added code for ArrayLists

---
+++ FirstProject/src/FirstPackage/ArrayLists.java
@@ -0,0 +1,21 @@
+package FirstPackage;
+
+import java.util.*;
+
+public class ArrayLists {
+	
+	static Scanner input = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		ArrayList<String> arr1 = new ArrayList<>();
+		
+		for(int i = 0 ; i < 5 ; i++) {
+			System.out.println("Enter String: ");String val = input.next();
+			arr1.add(val);
+		}
+		Collections.sort(arr1);
+		
+		System.out.println(arr1);
+	
+	}
+}
