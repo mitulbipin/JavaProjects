eefe75ad6362feed74c5906df8dcbdc623c60d33
Mitul Bipin <mitulbipin24@gmail.com>
2020-07-18 18:14:57 +0530

    Updated the code with Inheritance

---
+++ FirstProject/src/FirstPackage/Automobile_Inheritance.java
@@ -0,0 +1,73 @@
+package FirstPackage;
+
+import java.util.Scanner;
+
+class driverClass_Inheritance{
+	
+}
+
+class Automobile_Inheritance {
+	static Scanner input = new Scanner(System.in);
+	
+	public static String type;
+
+	public static String name;
+	public static int gears;
+
+	public int maxSpeed;
+	public double engineCapacity, horsePower, torque;
+	
+	public Automobile_Inheritance(int gears) {
+		this.gears = gears;
+	}
+
+		
+	public static void main(String args[]) {
+		System.out.println("Choose your input:\n1-Two Wheeler\n2-Four Wheeler");int option = input.nextInt();
+
+		switch(option) {
+		case 1: inputVariables(option);break;
+		case 2:inputVariables(option);break;
+		}
+	}
+	
+	public static void inputVariables(int option) {
+		if(option == 1) {
+			type = "Two Wheeler";
+			twoWheelers obj1 = new twoWheelers(name,gears); 
+			System.out.println(obj1.toString());
+			}
+		else {
+			type = "Four Wheeler";
+			fourWheelers obj2 = new fourWheelers(name,gears);
+			System.out.println(obj2.toString());
+		}
+		System.out.println("Input name of "+type);name = input.next();
+		System.out.println("Enter the gears in "+name);gears = input.nextInt();
+	}
+}
+class twoWheelers extends Automobile_Inheritance{
+
+	public twoWheelers(String name, int gears) {
+		super(gears);
+		// TODO Auto-generated constructor stub
+	}
+		private int rpm;
+		public int addRpm(int val) {
+			return val = rpm;
+		}
+		
+	public String toString() {
+		return(super.toString()+"\nName:"+name);
+	}
+}
+class fourWheelers extends Automobile_Inheritance{
+
+	public fourWheelers(String name, int gears) {
+		super(gears);
+		// TODO Auto-generated constructor stub
+	}
+	public String toString() {
+		return(super.toString()+"\nName:"+name);
+	}
+}
